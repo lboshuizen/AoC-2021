a9e62ba8f18c1bf3ee6d76f17c341ddad1ee7779
Leon Boshuizen <llc.boshuizen@gmail.com>
2021-12-05 22:12:39 +0100

    day4

--- src/Day4/Bingo.hs
+++ src/Day4/Bingo.hs
@@ -1,5 +1,7 @@
 module Day4.Bingo (bingo) where
 
+import Data.List (find, (\\))
+import Data.Maybe (mapMaybe)
 import Data.List.Split (splitWhen,splitOn)
 import Debug.Trace (trace)
 -- creates an XY map/chart
@@ -8,39 +10,63 @@ import Debug.Trace (trace)
 --                    ((0,1),'#'),((1,1),'.'),((2,1),'.')
 --                  ]
 -- Note: origin/xy:(0,0) is top left
-indexXY :: Integral n => [[a]] -> [((n,n),a)]
---indexXY :: [[a]] -> [((x,y),a)]
+
+type Card = [((Int,Int), Int)]
+
+indexXY :: [[a]] -> [((Int,Int),a)] --indexXY :: [[a]] -> [((x,y),a)]
 indexXY xs = concat $ [[((x, y), c) | (x, c) <- zip [0 ..] r] | (y, r) <- zip [0 ..] xs]
 
-row :: Integral n => [((n,n),a)] -> Int -> [a]
-row board r = [] 
-
-col :: Integral n => [((n,n),a)] -> Int -> [a]
-col board c = [] 
-
-marked :: Eq a => [a] -> a -> Bool 
-marked xs n = n `elem` xs
-
---winner :: Eq a => [a] -> [((n,n),a)] -> Bool
---winner xs = all (\n -> n elem xs)  . map snd
-
---cross :: Eq a => [a] -> [((n,n),a)] -> Bool
---cross xs = any (\n -> n elem xs)  . map snd
-
---parse :: Integral n => [String] -> ([Int],[((n,n),a)])
---parse :: [String] -> ([Int],[[String]])
-
-dropEmpty = splitWhen (=="")
-
-stoi s = read s :: Int
-
---parse :: [String] -> ([Int],[[String]])
-parse xs = (pool, boards)
+parse :: [String] -> ([Int],[Card])
+parse xs = (pool, cards)
     where 
+          stoi s = read s :: Int
           pool = map (stoi) . splitOn "," . head $ xs
-          boards = map (board) . splitWhen (=="") . drop 2 $ xs
-          board = indexXY . map (map (stoi) . words)
---scan :: Integral n => [[((n,n),a)]] -> n -> Maybe [a]
+          cards = map (card) . splitWhen (=="") . drop 2 $ xs
+          card = indexXY . map (map (stoi) . words)
 
---bingo :: [String] -> ([Int],[String])
-bingo  = parse
+numbers :: Card -> [Int]
+numbers = map (snd)
+
+row :: Int -> Card -> [Int]
+row r = numbers . filter (\((_,y),_) -> y == r)
+
+col :: Int -> Card -> [Int]
+col r = numbers . filter (\((x,_),_) -> x == r)
+
+full :: [Int] -> [Int] -> Bool
+full d = all (\n -> elem n d)
+
+sq :: Int -> Card -> Maybe ((Int,Int),Card)
+sq n c = case find (\((_,_),v) -> v == n) c of
+         Nothing -> Nothing
+         Just (xy,_) -> Just (xy, c)
+
+withSquare :: Int -> [Card] -> [((Int,Int), Card)]
+withSquare n = mapMaybe (sq n)
+
+fullRowOrCol :: [Int] -> ((Int,Int), Card) -> Bool
+fullRowOrCol draw ((x,y),crd) = (fullcol x) || (fullrow y)
+    where fullcol c = full draw . col c $ crd
+          fullrow r = full draw . row r $ crd
+
+score :: [Int] -> Card -> Int
+score draw@(l:_) = (*) l . sum . filter (\n -> notElem n draw) . numbers
+
+part1 :: ([Int],[Card]) -> Int
+part1 (ns,crds) = go ns []
+                  where go (n:xs) prev = case winners of
+                                         [] -> go xs (n:prev)
+                                         _  -> score (n:prev) . snd . head $ winners
+                                         where winners = filter (fullRowOrCol (n:prev)) . withSquare n $ crds
+
+part2 (ns,crds) = go ns [] crds
+    where
+        go (n:_) prev [c] = score (n:prev) $ c
+        go (n:xs) prev cs = case winners of
+                            [] -> go xs (n:prev) cs
+                            _  -> go xs (n:prev) (cs \\ (snd <$> winners))
+                            where winners = filter (fullRowOrCol (n:prev)) . withSquare n $ cs
+
+
+bingo :: [String] -> Int
+bingo = part2 . parse
